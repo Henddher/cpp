016d9fcb84eaa9e3bdf814771b951cf650b7a911
Henddher Pedroza <hpedroza@wje.com>
2018-10-12 08:35:04 -0500

    string copy-on-write experiments and string API usage

--- 01/main.cpp
+++ 01/main.cpp
@@ -1,12 +1,21 @@
-// Welcome to the Interactive C++ Tutorial.
-// Start by choosing a chapter and
-// write your code in this window.
-
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+    string s("Hello world \xF0\x9F\x98\x81");
+    string s2 = s;
+    string *sp = &s;
+    s2.replace(7, 3, " sup");
+    for (int i = 0; i < s.length(); i++) {
+        // if (i < s.length() && i > s.length() / 2) {
+        //     s[i] = 'x';
+        // }
+        cout << s.at(i) << sp->at(i) << s2.at(i) << endl;
+    }
+    cout << s << endl;
+    cout << *sp << endl;
+    cout << s2 << endl;
     return 0;
 }
 
